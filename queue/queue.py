dc15bfeafebed2fe7bde0b897cdd46ca2a0631d0
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-06-22 16:45:58 -0700

    working on DLL and BST

--- queue/queue.py
+++ queue/queue.py
@@ -43,6 +43,15 @@ class QueueLL:
     def __init__(self):
         self.size = 0
         self.storage = LinkedList()
+    
+    def enqueue(self, value):
+        self.size += 1
+        self.storage.add_to_tail(value)
+
+    def dequeue(self):
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_head()
 
     def len(self):
         return self.size
